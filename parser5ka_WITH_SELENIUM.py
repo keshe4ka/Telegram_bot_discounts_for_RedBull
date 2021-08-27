ff4b5b00396f22aba84d2220c67c861c3be282f8
keshe4ka <artem.anaschenko@gmail.com>
2021-08-27 23:51:57 +0300

    Убрали селениум, в едадиле пропали скидки на редбулл...

--- parser5ka_WITH_SELENIUM.py
+++ parser5ka_WITH_SELENIUM.py
@@ -1,15 +1,21 @@
 import re
 
-from requests_html import HTMLSession
+from selenium import webdriver
 from bs4 import BeautifulSoup
 from config import shop_5ka_url
 from time import sleep
 
-session = HTMLSession()
-response = session.get(shop_5ka_url)
-sleep(3)  # ожидание прогрузки страницы
+options = webdriver.FirefoxOptions()
+options.add_argument('--headless')
 
-soup = BeautifulSoup(response.content, 'html.parser')
+browser = webdriver.Firefox(firefox_options=options)
+browser.get(shop_5ka_url)
+sleep(3)  # ожидание прогрузки страницы
+generatedHTML = browser.page_source
+
+browser.quit()
+
+soup = BeautifulSoup(generatedHTML, 'html.parser')
 
 try:
     title = soup.find('div', class_='b-offer__description')
@@ -20,4 +26,4 @@ try:
     price1 = price[:-2]
     info = "\nПЯТЕРОЧКА: " + price1 + "." + price2 + "р."
 except:
-    info = "\nПЯТЕРОЧКА: скидки нет"
+    info = "\nПЯТЕРОЧКА: скидок нет"
