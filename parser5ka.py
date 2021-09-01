cc9b51f0413ce26ab9bbe5da73bd24751d17f17d
keshe4ka <artem.anaschenko@gmail.com>
2021-09-02 01:42:23 +0300

    Отказывайтесь от requests пацаны... страницы до конца не прогружает...

--- parser5ka.py
+++ parser5ka.py
@@ -5,13 +5,12 @@ from parser import parse
 
 soup = parse(shop_5ka_url, 3)
 
-try:
-    title = soup.find('div', class_='b-offer__description')
-    checkDiscountOnBigCan = title.find('0,473')
-    print(checkDiscountOnBigCan)
-    price = re.sub('\D', '', soup.find('div', class_='b-offer__price-new').text)
-    price2 = price[-2:]
-    price1 = price[:-2]
-    info = "\nПЯТЕРОЧКА: " + price1 + "." + price2 + "р."
-except:
-    info = "\nПЯТЕРОЧКА: скидки нет"
+volume = soup.select('.b-offer__description')
+# checkDiscountOnBigCan = title.find('0,25')
+print(volume)
+price = re.sub('\D', '', soup.find('div', class_='b-offer__price-new').text)
+price2 = price[-2:]
+price1 = price[:-2]
+info = "\nПЯТЕРОЧКА: " + price1 + "." + price2 + "р."
+
+#    info = "\nПЯТЕРОЧКА: скидки нет"
